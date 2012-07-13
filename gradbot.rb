13647e4aa2a1b9cee223c83fa0074669b7e11423
Sam Starling <sam.starling@gmail.com>
2012-07-13 13:36:12 +0100

    Initial import

---
+++ gradbot.rb
@@ -0,0 +1,20 @@
+require 'cinch'
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server = '127.0.0.1'
+    c.port = '6697'
+    c.nick = 'gradbot'
+    c.channels = ['#thegrads']
+  end
+  
+  on :join do |m|
+    m.reply "A wild #{m.user.nick} appeared!"
+  end
+
+  on :message, "!londonlunch" do |m|
+    m.reply "samstarling, MikeSpelling, PaulL: Lunch!"
+  end
+end
+
+bot.start
