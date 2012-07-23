2ee9d204975e147e4e72597408fd5c1c2f2758ff
Sam Starling <sam.starling@gmail.com>
2012-07-23 11:10:45 +0100

    Karma no longer gets overwritten

--- plugins/karma.rb
+++ plugins/karma.rb
@@ -9,7 +9,7 @@ class Karma
 	match /([\w]+)--/, method: :remove_karma, use_prefix: false
 
 	def add_karma(m, arg)
-		@karma ||= load_hash
+		load_hash
 		@karma[arg.to_sym] ||= 0
 		val = @karma[arg.to_sym] += 1
 		m.reply "Gave more karma to \"#{arg}\". New karma: #{val}"
@@ -17,7 +17,7 @@ class Karma
 	end
 
 	def remove_karma(m, arg)
-		@karma ||= load_hash
+		load_hash
 		@karma[arg.to_sym] ||= 0
 		val = @karma[arg.to_sym] -= 1
 		m.reply "Took karma away from \"#{arg}\". New karma: #{val}."
@@ -25,20 +25,20 @@ class Karma
 	end
 	
 	def load_hash
-		@karma ||= Hash.new
-		save_hash
-		File.open('karma') do |f|
-			@karma = Marshal.load(f)
-		end
-		return @karma
-	end
-	
-	def save_hash
-		unless File.exists?('karma')
+    if File.exists?('karma')
+      @karma = Marshal.load(f)
+    else
+      @karma = Hash.new
 			File.new('karma', 'w')
-		end
-		File.open('karma', 'w+') do |f|
-			Marshal.dump(@karma, f)
-		end
-	end
+      File.open('karma', 'w+') do |f|
+			  Marshal.dump(@karma, f)
+		  end
+    end
+  end
+
+  def save_hash
+    File.open('karma', 'w+') do |f|
+      Marshal.dump(@karma, f)
+    end
+  end
 end
