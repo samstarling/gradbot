8297f700734c7eff6d85aba28a6db739d32f057b
Sam Starling <sam.starling@gmail.com>
2012-07-23 13:51:12 +0100

    Fixing file read

--- plugins/karma.rb
+++ plugins/karma.rb
@@ -3,36 +3,38 @@ require 'cinch'
 require 'json'
 
 class Karma
-	include Cinch::Plugin
+  include Cinch::Plugin
 
-	match /([\w]+)\+\+/, method: :add_karma, use_prefix: false
-	match /([\w]+)--/, method: :remove_karma, use_prefix: false
+  match /([\w]+)\+\+/, method: :add_karma, use_prefix: false
+  match /([\w]+)--/, method: :remove_karma, use_prefix: false
 
-	def add_karma(m, arg)
-		load_hash
-		@karma[arg.to_sym] ||= 0
-		val = @karma[arg.to_sym] += 1
-		m.reply "Gave more karma to \"#{arg}\". New karma: #{val}"
-		save_hash
-	end
+  def add_karma(m, arg)
+    load_hash
+    @karma[arg.to_sym] ||= 0
+    val = @karma[arg.to_sym] += 1
+    m.reply "Gave more karma to \"#{arg}\". New karma: #{val}"
+    save_hash
+  end
 
-	def remove_karma(m, arg)
-		load_hash
-		@karma[arg.to_sym] ||= 0
-		val = @karma[arg.to_sym] -= 1
-		m.reply "Took karma away from \"#{arg}\". New karma: #{val}."
-		save_hash
-	end
-	
-	def load_hash
+  def remove_karma(m, arg)
+    load_hash
+    @karma[arg.to_sym] ||= 0
+    val = @karma[arg.to_sym] -= 1
+    m.reply "Took karma away from \"#{arg}\". New karma: #{val}."
+    save_hash
+  end
+
+  def load_hash
     if File.exists?('karma')
-      @karma = Marshal.load(f)
+      File.open('karma') do |f|
+        @karma = Marshal.load(f)
+      end
     else
       @karma = Hash.new
-			File.new('karma', 'w')
+      File.new('karma', 'w')
       File.open('karma', 'w+') do |f|
-			  Marshal.dump(@karma, f)
-		  end
+        Marshal.dump(@karma, f)
+      end
     end
   end
 
