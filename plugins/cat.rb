579b728b0fd34d53d5bc5e4c141000b4a2b9e0a4
Sam Starling <sam.starling@gmail.com>
2012-09-06 20:59:59 +0100

    Adding rspec, and some tests for the Cat plugin

--- plugins/cat.rb
+++ plugins/cat.rb
@@ -1,5 +1,6 @@
 require 'cinch'
 require 'nokogiri'
+require 'rest-client'
 
 class Cat
   include Cinch::Plugin
@@ -8,6 +9,9 @@ class Cat
   
   def execute(m)
     xml = Nokogiri::XML(RestClient.get 'http://thecatapi.com/api/images/get?format=xml&results_per_page=1')
-    m.reply xml.xpath('//url').text
+    url = xml.xpath('//url')
+    if not url.empty?
+      m.reply url.text
+    end
   end
 end
---
+++ spec/cat_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../plugins/cat.rb'
+require 'rest-client'
+
+describe Cat do
+  
+  before(:each) do
+    config = double('config').as_null_object
+    @cat = Cat.new(config)
+    @message = double('message')
+    RestClient.stub(:get).and_return('<url>cat</url>')
+  end
+  
+  it "should reply to messages" do
+    @message.should_receive(:reply)
+    @cat.execute(@message)
+  end
+  
+  it "should extract images from the cat API" do
+    @message.should_receive(:reply).with('cat')
+    @cat.execute(@message)
+  end
+  
+  it "shouldn't reply when there's no image" do
+    RestClient.stub(:get).and_return('<foo>bar</foo>')
+    @message.should_not_receive(:reply)
+    @cat.execute(@message)
+  end
+  
+end
