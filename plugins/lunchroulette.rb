f5b1891e2fc529ca8454685c934b54c050e3b2ad
Sam Starling <sam.starling@gmail.com>
2012-09-07 09:52:39 +0100

    LunchRoulette fix

--- plugins/lunchroulette.rb
+++ plugins/lunchroulette.rb
@@ -8,11 +8,15 @@ class LunchRoulette
   @@VENUES = {
     "w12" => ["Leon", "Pret", "Kaffiene", "Boots"],
     "w1" => ["Canteen", "Tesco", "Davy's", "Gourmet Burger Kitchen", "Busaba Eathai"],
-    "salford" => ["Canteen", "Wagamama", "Booths", "You must go swimming in the canal instead of lunch"]
+    "salford" => ["Canteen", "Wagamama", "Booths", "you must go swimming in the canal instead of lunch"]
   }
   
   def execute(m, postcode)
     venue = @@VENUES[postcode].sample
-    m.reply("#{m.user.nick}, you must lunch at '#{venue}'")
+    if venue.include? 'canal'
+      m.reply("#{m.user.nick}, #{venue}")
+    else
+      m.reply("#{m.user.nick}, you must lunch at '#{venue}'")
+    end
   end
 end
