5308a016783a4bc866fb1f430c6d448d5c87708b
Sam Starling <sam.starling@gmail.com>
2012-09-24 13:03:11 +0100

    Adding more places

--- plugins/weather.rb
+++ plugins/weather.rb
@@ -10,13 +10,15 @@ class Weather
 
   LOCATIONS = {
     :london => 44418,
-    :salford => 33887
+    :salford => 33887,
+    :manchester => 28218,
+    :miami => 2450022
   }
   
   match /weather (.+)/
 
   def location_to_id(location)
-    LOCATIONS[location.strip.downcase.to_sym]
+    LOCATIONS[location.strip.downcase.to_sym] || nil
   end
 
   def f_to_c f
@@ -25,8 +27,11 @@ class Weather
   
   def execute(m, location)
     location_id = location_to_id location
-    response = JSON.parse(RestClient.get "http://weather.yahooapis.com/forecastjson?w=#{location_id}")
-    celsius = f_to_c response['condition']['temperature']
-    m.reply "#{m.user.nick}: #{response['condition']['text']}, #{celsius}°C"
+    if location_id
+      response = JSON.parse(RestClient.get "http://weather.yahooapis.com/forecastjson?w=#{location_id}")
+      celsius = f_to_c response['condition']['temperature']
+      m.reply "#{m.user.nick}: #{response['condition']['text']}, #{celsius}C"
+    else
+      m.reply "#{m.user.nick}: I'm not aware of '#{location}'"
   end
 end
