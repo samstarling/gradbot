f04b0873b50167e9a591a6e2b5f523936840c172
Sam Starling <sam.starling@gmail.com>
2012-09-28 16:35:57 +0100

    Decoupling Cat plugin

--- plugins/cat.rb
+++ plugins/cat.rb
@@ -1,17 +1,26 @@
 require 'nokogiri'
 require 'rest-client'
 
+class CatApiClient
+  def self.get_cat
+    url = 'http://thecatapi.com/api/images/get?format=xml&results_per_page=1'
+    xml = Nokogiri::XML(RestClient.get url)
+    element = xml.xpath('//url')
+    if element && element.text != ""
+      element.text
+    else
+      nil
+    end
+  end
+end
+
 class Cat
   include Cinch::Plugin
 
   match /cat/
 
   def execute(m)
-    url = 'http://thecatapi.com/api/images/get?format=xml&results_per_page=1'
-    xml = Nokogiri::XML(RestClient.get url)
-    cat = xml.xpath('//url')
-    if not cat.empty?
-      m.reply cat.text
-    end
+    cat = CatApiClient.get_cat
+    m.reply cat if cat
   end
 end
