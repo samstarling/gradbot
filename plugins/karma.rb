a6aa57159df0b626b6db341ce5f1449644139032
Mike <mike.spelling@hotmail.co.uk>
2012-10-01 11:09:46 +0100

    Added += method to karma plugin

--- plugins/karma.rb
+++ plugins/karma.rb
@@ -2,43 +2,125 @@ require 'rest-client'
 require 'cinch'
 require 'json'
 
+class KarmaData
+  attr_accessor :data 
+
+  def initialize
+    @filepath = ENV['karma_file'] || '/home/samstarling/temp/karma.marshal'
+    load
+  end
+
+  def save
+    File.open(@filepath, 'w') do |f|
+      Marshal.dump(@data, f)
+    end
+  end
+
+  def regroup
+    regrouped = Hash.new
+    @data.each do |thing, score|
+      (regrouped[score] ||= []) << thing if score != 0
+    end
+    regrouped
+  end
+
+  def reset
+    @data = Hash.new
+    save
+  end
+  
+  private
+  
+  def load
+    if File.exists?(@filepath)
+      File.open(@filepath) do |f|
+        @data = Marshal.load(f)
+      end
+    else
+      @data = Hash.new
+      save
+    end
+  end
+end
+
 class Karma
-	include Cinch::Plugin
+  include Cinch::Plugin
 
-	match /([\w]+)\+\+/, method: :add_karma, use_prefix: false
-	match /([\w]+)--/, method: :remove_karma, use_prefix: false
+  match /karma/
+  match /([\w]+)\+\+/, method: :add_karma, use_prefix: false
+  match /([\w]+)\+=(\d+)/, method: :add_x_karma, use_prefix: false
+  match /([\w]+)--/, method: :remove_karma, use_prefix: false
+  match /([\w]+)\-=(\d+)/, method: :remove_x_karma, use_prefix: false
+  listen_to :connect
+  listen_to :join, method: :startup
 
-	def add_karma(m, arg)
-		@karma ||= load_hash
-		@karma[arg.to_sym] ||= 0
-		val = @karma[arg.to_sym] += 1
-		m.reply "Gave more karma to \"#{arg}\". New karma: #{val}"
-		save_hash
-	end
+  def startup(m=nil)
+    @data_source = KarmaData.new
+  end
 
-	def remove_karma(m, arg)
-		@karma ||= load_hash
-		@karma[arg.to_sym] ||= 0
-		val = @karma[arg.to_sym] -= 1
-		m.reply "Took karma away from \"#{arg}\". New karma: #{val}."
-		save_hash
-	end
-	
-	def load_hash
-		@karma ||= Hash.new
-		save_hash
-		File.open('karma') do |f|
-			@karma = Marshal.load(f)
-		end
-		return @karma
-	end
-	
-	def save_hash
-		unless File.exists?('karma')
-			File.new('karma', 'w')
-		end
-		File.open('karma', 'w+') do |f|
-			Marshal.dump(@karma, f)
-		end
-	end
+  def get_value thing
+    @data_source.data[thing]
+  end
+
+  def execute(m)
+    @data_source.regroup.each do |score, things|
+      possession = if things.size == 1 then "has" else "have" end
+      thing_list = to_sentence things
+      m.reply "#{thing_list} #{possession} #{score} karma"
+    end
+  end
+
+  def to_sentence things
+    if things.size == 1
+      things.join ", "
+    else
+      start = things[0..-2].join ", "
+      finish = things[-1]
+      "#{start} and #{finish}"
+    end
+  end
+
+  def reset_karma
+    @data_source.reset
+  end
+
+  def add_karma(m, arg)
+    arg.downcase!
+    @data_source.data[arg.to_sym] ||= 0
+    val = @data_source.data[arg.to_sym] += 1
+    noise = ["Boom", "Ping", "Bam", "Smash", "Wahey", "Yay"].sample
+    action = "#{m.user.nick} gave more karma to \"#{arg}\""
+    m.reply "#{noise}! #{action}. New karma: #{val}"
+    @data_source.save
+  end
+
+  def add_x_karma(m, arg, x)
+    arg.downcase!
+    @data_source.data[arg.to_sym] ||= 0
+    val = @data_source.data[arg.to_sym] += x.to_i
+    noise = ["Boom", "Ping", "Bam", "Smash", "Wahey", "Yay"].sample
+    action = "#{m.user.nick} gave more karma to \"#{arg}\""
+    m.reply "#{noise}! #{action}. New karma: #{val}"
+    @data_source.save
+  end
+
+  def remove_karma(m, arg)
+    arg.downcase!
+    @data_source.data[arg.to_sym] ||= 0
+    val = @data_source.data[arg.to_sym] -= 1
+    noise = ["Oh dear", "O noes", "Erk", "Oops", "Sadface"].sample
+    action = "#{m.user.nick} took karma away from \"#{arg}\""
+    m.reply "#{noise}. #{action}. New karma: #{val}."
+    @data_source.save
+  end
+
+  def remove_karma(m, arg, x)
+    arg.downcase!
+    @data_source.data[arg.to_sym] ||= 0
+    val = @data_source.data[arg.to_sym] -= x.to_i
+    noise = ["Oh dear", "O noes", "Erk", "Oops", "Sadface"].sample
+    action = "#{m.user.nick} took karma away from \"#{arg}\""
+    m.reply "#{noise}. #{action}. New karma: #{val}."
+    @data_source.save
+  end
 end
