5b6d4d10247f79fb45fbfc22021ac1034073c08c
Sam Starling <sam.starling@gmail.com>
2012-10-02 14:10:54 +0100

    Fix

--- plugins/celebtracker.rb
+++ plugins/celebtracker.rb
@@ -14,7 +14,7 @@ class GoogleClient
   private
   
   def self.get_result term
-    safe_term = "#{URI.escape(term)}
+    safe_term = URI.escape(term)
     JSON.parse(RestClient.get "#{BASE_URL}?v=1.0&q=#{safe_term}")
   end
 end
