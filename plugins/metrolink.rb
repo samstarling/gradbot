88748785bdea3c77197dd3b33d8c08924e707410
Chris Northwood <chris@pling.org.uk>
2012-11-14 16:40:24 +0000

    Update following Metrolink URL change

--- plugins/metrolink.rb
+++ plugins/metrolink.rb
@@ -7,7 +7,7 @@ class Metrolink
   match /tram/
 
   def execute(m)
-    url = "http://www.metrolink.co.uk/todaysdisruptions/index.asp?id=25"
+    url = "http://www.metrolink.co.uk/pages/news.aspx?serviceID=39"
     response = Nokogiri::HTML.parse(RestClient.get(url))
     heading = response.css("h3.pageHeading").first.text
     m.reply heading
