65fee32871436d2689594eaafc6b294e0df95d37
Sam Starling <sam.starling@gmail.com>
2012-11-28 12:42:30 +0000

    Upping the TWSS threshold

--- plugins/twss.rb
+++ plugins/twss.rb
@@ -6,6 +6,7 @@ class ThatsWhatSheSaid
   listen_to :message, :method => :twss
 
   def twss(m, *args)
+    TWSS.threshold = 8.0
     if TWSS(m.message)
       sleep 1
       m.reply "#{m.user.nick}: That's what she said!"
