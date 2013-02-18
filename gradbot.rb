b41124d1f42b8fa5a4543bf35805257ae3597916
Sam Starling <sam.starling@gmail.com>
2013-02-18 16:53:49 +0000

    Change nick

--- gradbot.rb
+++ gradbot.rb
@@ -17,7 +17,8 @@ bot = Cinch::Bot.new do
       CelebTracker,
       Metrolink,
       News,
-      Anonymous
+      Anonymous,
+      Nick
     ]
     c.ssl.use = true
     c.ssl.verify = false
--- plugins/nick.rb
+++ plugins/nick.rb
@@ -1,4 +1,4 @@
-class News
+class Nick
   include Cinch::Plugin
 
   match /nick (\w+)/
